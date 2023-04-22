16b2b5d0faaa45b72110c84d7a8299c5e7190e03
onkelzeddy <neckit.nic@yandex.ru>
2023-04-22 15:40:25 +0300

    Update trapezoid.cpp

--- modules/task_2/mironov_n_trapezoid_method_omp/trapezoid.cpp
+++ modules/task_2/mironov_n_trapezoid_method_omp/trapezoid.cpp
@@ -172,8 +172,7 @@ double d2_method_Openmp(
     double y = 0;
 
     double result = 0;
-    std::_Pragma(R"omp parallel private(i, j, x, y)
-    shared(N, bounds, h_for_x, h_for_y, result)") {
+    #pragma omp parallel shared(N, bounds, h_for_x, h_for_y, result) {
         #pragma omp for nowait reduction(+: result)
         for (i = 1; i <= N; i++) {
             x = bounds[0].first + h_for_x * i;
@@ -225,9 +224,7 @@ double d3_method_Openmp(
     double z = 0;
 
     double result = 0;
-    #pragma omp \
-    parallel private(i, j, s, x, y) \
-    shared(N, bounds, h_for_x, h_for_y, h_for_z, result) {
+    #pragma omp parallel shared(N, bounds, h_for_x, h_for_y, h_for_z, result) {
         #pragma omp for nowait reduction(+: result)
         for (i = 1; i <= N; i++) {
             x = bounds[0].first + h_for_x * i;
